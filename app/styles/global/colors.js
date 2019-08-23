61ce29e11dc163baeb9eed86e7605aabe747b845
jrammmy <justinramiscal@gmail.com>
2019-08-22 23:06:16 -0700

    cleaning out the styles

--- app/styles/global/colors.js
+++ app/styles/global/colors.js
@@ -1,43 +1,52 @@
-let offWhite = '#E3E3E3';
-let darkGray = '#323337';
+let offWhite = "#E3E3E3";
+let darkGray = "#323337";
 
 //Different shades of red
-export let red_shades = [ '#2C699B', '#2CA5FD',"#035795", "#035795","#035795","#035795","#035795","#035795","#035795","#035795","#035795","#035795","#035795","#035795","#035795","#035795","#035795","#035795"];
-
+export let red_shades = [
+  "#2C699B",
+  "#2CA5FD",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795",
+  "#035795"
+];
 
 export const Colors = {
-	black: '#000000',
-	white:'#ffffff',
-	gray: '#777777',
-	defaultBackground: '#DFF0FB',
-	shadow: '#cccccc',
-	darkGray: '#323337',
-	red: '#FF5858',
+  black: "#000000",
+  white: "#ffffff",
+  gray: "#777777",
+  defaultBackground: "#DFF0FB",
+  shadow: "#cccccc",
+  darkGray: "#323337",
+  red: "#FF5858",
 
+  //dark mode
+  darkBlue: "#035795",
+  blue: "#2C699B",
+  lightBlue: "#2CA5FD",
+  robinHoodGreen: "#21CE99",
+  offWhite: "#E3E3E3",
+  lightGray: "#797979",
+  mediumDarkGray: "#3B3F42",
+  darkGrayBackground: "#171717",
+  darkRed: "#8b0000",
+  Darkred: "#B92E26",
+  green: "#12CC00",
 
-//dark mode
-	darkBlue: "#035795",
-	blue: '#2C699B',
-	lightBlue: '#2CA5FD',
-	robinHoodGreen: "#21CE99",
-	offWhite: '#E3E3E3',
-	lightGray: '#797979',
-	mediumDarkGray: '#3B3F42',
-
-	darkGrayBackground: '#171717',
-
-	darkRed: '#8b0000',
-
-	Darkred: '#B92E26',
-	green: '#12CC00',
-
-
-	//CRYPTANNA Colors
-	c_dark_red : '#5B0E08',
-	c_ligth_red : '#881910',
-	c_light2_red : '#A51B23',
-
-
-
-
+  //CRYPTANNA Colors
+  c_dark_red: "#5B0E08",
+  c_ligth_red: "#881910",
+  c_light2_red: "#A51B23"
 };
