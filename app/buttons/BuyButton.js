6d5fe09ddef7eb55b667c3e8d03b07879f1ad137
Elliot Eckholm <ellioteckholm@gmail.com>
2020-01-19 21:24:26 -0800

    added working buy button logic and adds to firebase successfully

--- app/buttons/BuyButton.js
+++ app/buttons/BuyButton.js
@@ -4,6 +4,7 @@ import { Icon } from 'react-native-elements';
 import Markets from '../screens/Markets.js';
 import styles from '../styles/BuyButton.style.js';
 import ccxt from 'ccxt';
+import Spinner from "./../config/Spinner";
 import firebase from 'react-native-firebase';
 
 /////////////Functionality imports//////////
@@ -16,63 +17,112 @@ export default class BuyButton extends Component {
       this.state = {
         marketObj:[],
         balanceList: [],
-        buy_amount: "",
+        buyAmount: 0.0,
         currentPrice_string: "",
-        currentPrice: this.props.currentPrice,
+        currentPrice: 0.0,
         sandboxObject: this.props.sandboxObject,
+        currentPriceLoading: true,
 
       };
     }
 
+    waitForTickerFetch = async()=> {
+
+      let market = "BTC/USD";
+
+      let exchangeTitle = "coinbasepro";
+      let exchange = new ccxt[exchangeTitle]();
+      let marketInfo = {};
+
+
+      fetchTicker(exchange, market, marketInfo)
+        .then(() => {
+          marketLoaded = true;
+          this.setState(previousState => {
+            return { marketObj: marketInfo };
+          });
+
+          this.setState({
+            currentPrice: parseFloat(marketInfo.info.price).toFixed(2),
+            currentPriceLoading: false,
+
+          });
+
+
+          console.log("Buy Amount: ",this.state.buyAmount);
+          console.log("In buy button Current Price: ", this.state.currentPrice);
+
+
+          let new_usd_balance = parseFloat(this.state.sandboxObject.current_usd_balance) - this.state.buyAmount;
+          let new_btc_balance =  this.state.buyAmount / parseFloat(this.state.currentPrice);
+
+          this.state.sandboxObject.current_usd_balance = parseFloat(new_usd_balance);
+          this.state.sandboxObject.current_btc_balance +=  parseFloat(new_btc_balance);
+
+
+          console.log("New USD balance: ",new_usd_balance);
+          console.log("New BTC balance: ",new_btc_balance);
+           // Alert.alert(
+           //   "You dont have enough US Dollars to buy that amount of Bitcoin!",
+           //   'Please Try Again',
+           //   [
+           //     {
+           //       text: 'Cancel',
+           //       onPress: () => console.log('Cancel Pressed'),
+           //       style: 'cancel'
+           //     },
+           //     { text: 'OK', onPress: () => console.log('OK Pressed') }
+           //   ],
+           //   { cancelable: false }
+           // );
+
+         writeSandBoxBalance(this.state.sandboxObject);
+         this.setState({
+           buyAmount: ""
+         });
+
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+
     componentDidMount() {
 
-      console.log("In buy button USD Balance: ", this.state.sandboxObject.current_usd_balance);
-      console.log("In buy button BTC Balance: ", this.state.sandboxObject.current_btc_balance);
-      console.log("In buy button Current Price: ", this.state.currentPrice);
+      // console.log("In buy button USD Balance: ", this.state.sandboxObject.current_usd_balance);
+      // console.log("In buy button BTC Balance: ", this.state.sandboxObject.current_btc_balance);
+      //
+      //
+      //
+
+
 
     }
 
     _onBuy = () => {
 
-
-         new_btc_balance = 0.2;
-         new_usd_balance = 10000;
-         this.state.sandboxObject.current_usd_balance = new_usd_balance;
-         this.state.sandboxObject.current_btc_balance = new_btc_balance;
-
-
-         // Alert.alert(
-         //   "You dont have enough US Dollars to buy that amount of Bitcoin!",
-         //   'Please Try Again',
-         //   [
-         //     {
-         //       text: 'Cancel',
-         //       onPress: () => console.log('Cancel Pressed'),
-         //       style: 'cancel'
-         //     },
-         //     { text: 'OK', onPress: () => console.log('OK Pressed') }
-         //   ],
-         //   { cancelable: false }
-         // );
-
-       writeSandBoxBalance(this.state.sandboxObject);
-
-       this.setState({
-           buy_amount: "",
-       });
+        this.waitForTickerFetch();
 
     }
 
     render() {
+
+      // if (this.state.currentPriceLoading){
+      //   return (
+      //     <View style={{ paddingTop: "50%" }}>
+      //       <Spinner />
+      //     </View>
+      //   );
+      // }else{
         return(
 
             <View style={styles.buttons_container}>
 
                 <TextInput style={styles.amountInput}
-                  onChangeText={(buy_amount) => this.setState({buy_amount : buy_amount})}
-                  value={this.state.buy_amount}
+                  onChangeText={(buyAmount) => this.setState({buyAmount : parseFloat(buyAmount)})}
+                  value={this.state.buyAmount}
                   placeholderTextColor={'white'}
-                  placeholder = "0.0"
+                  placeholder = "$ 0.0"
                 />
 
               <TouchableOpacity onPress={this._onBuy}>
@@ -87,5 +137,6 @@ export default class BuyButton extends Component {
             </View>
 
         );
+      // }
     }
 }
